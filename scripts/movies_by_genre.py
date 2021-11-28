48e22d680751cb782971b08e72b11a785fadc853
GitWAMH <61278893+GitWAMH@users.noreply.github.com>
2021-11-28 04:08:13 +0100

    Scripts

--- scripts/movies_by_genre.py
+++ scripts/movies_by_genre.py
@@ -1,5 +1,20 @@
 from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
 import sys
+import re
 
 conf = SparkConf().setAppName('Movies_by_genre')
 sc = SparkContext(conf=conf)
+spark = SparkSession.builder.master('local').appName('SparkSQL').getOrCreate()
+
+df1 = spark.read.option('header','true').csv('ratings.csv')
+df2 = spark.read.option('header','true').csv('movies.csv')
+
+df3 = df1.join(df2,'movieId','rightouter') \
+	.withColumn('genre',explode(split(col('genres'),'\\|'))) \
+	.withColumn('rating', df1['rating'].cast('float')) \
+	.select('movieId', 'rating', 'title', 'genre') \
+	.filter(col('genre')=='Action') \
+	.groupBy('movieId','title').avg('rating').alias('avg_rating') \
+	.show()
