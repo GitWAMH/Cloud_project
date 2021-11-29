48581bbe3cb34bb7b68ac765d3bab0e6cc0a77fe
GitWAMH <61278893+GitWAMH@users.noreply.github.com>
2021-11-29 18:06:21 +0100

    "Recommend movies by genre" functionality implemented

--- scripts/movies_by_genre.py
+++ scripts/movies_by_genre.py
@@ -8,13 +8,22 @@ conf = SparkConf().setAppName('Movies_by_genre')
 sc = SparkContext(conf=conf)
 spark = SparkSession.builder.master('local').appName('SparkSQL').getOrCreate()
 
+genre = sys.argv[1]
+movies_to_show = 5 if len(sys.argv)<3 else int(sys.argv[2])
+
 df1 = spark.read.option('header','true').csv('ratings.csv')
 df2 = spark.read.option('header','true').csv('movies.csv')
 
-df3 = df1.join(df2,'movieId','rightouter') \
+list_movies = df1.join(df2,'movieId','rightouter') \
 	.withColumn('genre',explode(split(col('genres'),'\\|'))) \
 	.withColumn('rating', df1['rating'].cast('float')) \
 	.select('movieId', 'rating', 'title', 'genre') \
-	.filter(col('genre')=='Action') \
-	.groupBy('movieId','title').avg('rating').alias('avg_rating') \
-	.show()
+	.filter(col('genre')==genre) \
+	.groupBy('movieId','title').avg('rating') \
+	.orderBy(col('avg(rating)').desc()) \
+	.limit(movies_to_show) \
+	.rdd \
+	.map(lambda row: (row[1],row[2])) \
+	.collect()
+
+print(list_movies)
