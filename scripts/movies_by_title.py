f2438164172fea6324f392f4e00255bb37707c77
robertotejedor <79274990+robertotejedor@users.noreply.github.com>
2021-12-03 12:33:40 +0100

    Search results sorted by rating

--- scripts/movies_by_title.py
+++ scripts/movies_by_title.py
@@ -1,5 +1,5 @@
-from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import lower
 import sys
 
 spark = SparkSession\
@@ -10,25 +10,33 @@ spark = SparkSession\
 
 if len(sys.argv) != 2:
 
-	print('Usage: spark-submit movies_by_title.py [\'movie name\']\n*Remember to type your movie name between quotes (\'\')')
+	print('Usage: spark-submit movies_by_title.py [\'movie title\']\n*Remember to type your movie title between quotes (\'\')')
 	
 else:
 	title = sys.argv[1]
 
 	#Search in CSV
-	df = spark.read.csv('movies.csv', header = 'true')
-
-	matchedMovies = df.select('movieId', 'title')\
-					.where(df['title'].contains(title))
+	moviesDF = spark.read.csv('movies.csv', header = True)
+	ratingsDF = spark.read.csv('ratings.csv', header = True)
+	matchedMovies = moviesDF.join(ratingsDF, 'movieId')\
+			.withColumn('rating', ratingsDF.rating.cast('float'))\
+			.where(lower(moviesDF.title).contains(title.lower()))\
+			.groupBy('movieId', 'title').avg('rating')\
+			.sort('avg(rating)', ascending = False)
 	
 	if not matchedMovies.rdd.isEmpty():
 		for i in matchedMovies.collect():
 			print(i[0] + ', ' + i [1])
 	#if it's not found in movies.csv, we search in title.basics.tsv
 	else:
-		df2 = spark.read.csv('title.basics.tsv', sep = r'\t', header = True)
-		matchedMovies = df2.select('tconst', 'primaryTitle', 'originalTitle')\
-						.where(df2['primaryTitle'].contains(title) | df2['originalTitle'].contains(title))
+		moviesDF = spark.read.csv('title.basics.tsv', sep = r'\t', header = True)
+		ratingsDF = spark.read.csv('title.ratings.tsv', sep = r'\t', header = True)
+		matchedMovies = moviesDF.join(ratingsDF, 'tconst')\
+				.withColumn('averageRating', ratingsDF.averageRating.cast('float'))\
+				.select('tconst', 'primaryTitle', 'originalTitle', 'averageRating')\
+				.where(lower(moviesDF.primaryTitle).contains(title.lower()) | lower(moviesDF.originalTitle).contains(title.lower()))\
+				.sort('averageRating', ascending = False)
+		
 		if not matchedMovies.rdd.isEmpty():
 			for i in matchedMovies.collect():
 				print(i[0] + ', ' + i [1] + ', ' + i[2])
