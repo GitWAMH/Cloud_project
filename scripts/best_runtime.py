a9e89fe0a3951bbf09c49cee394e65fed7867761
Lukas <Lukas-Sebastian.Hofmann@gmx.de>
2021-12-04 20:45:49 +0100

    finished_runtime

--- scripts/best_runtime.py
+++ scripts/best_runtime.py
@@ -3,55 +3,113 @@ from pyspark import *
 import sys
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
+from pyspark.sql.types import StringType, IntegerType, DoubleType, BooleanType
+from pyspark.sql.functions import col
+
 
 spark = SparkSession.builder \
         .master("local") \
-        .appName("Decision Pelicula") \
+        .appName("best_runtime") \
         .config("spark.some.config.option", "some-value") \
         .getOrCreate()
-df1 = spark.read.option('header', 'true').csv("title.akas.tsv", sep=r'\t')
-df2 = spark.read.option('header','true').csv("title.ratings.tsv", sep=r'\t') 
-df3 = spark.read.option('header','true').csv("title.basics.tsv", sep=r'\t') 
 
-df1.printSchema(), df2.printSchema(), df3.printSchema()
-joined_df = df1.join(df2, df1.titleId == df2.tconst, "left_semi").join(df3, df1.titleId == df3.tconst, "left_semi")
-#Insert drop for unneccasry columns 
-#Insert a distinct
-df1.unpersist(), df2.unpersist(), df3.unpersist()
-#Transform the data in the correct types
-DF = joined_df.withColumn("Open", joined_df["Open"].cast(DoubleType()))\
-                .withColumn("Open", joined_df["Open"].cast(DoubleType()))\
-                .withColumn("Open", joined_df["Open"].cast(DoubleType()))\
-                .withColumn("Open", joined_df["Open"].cast(DoubleType()))\
-                .withColumn("Open", joined_df["Open"].cast(DoubleType()))\
-                .withColumn("Open", joined_df["Open"].cast(DoubleType()))\
-                .withColumn("Open", joined_df["Open"].cast(DoubleType()))\
-                .distinct()
-print(joined_df.collect())
-joined_df.unpersist()
+df1_drop_cols = ("attributes", "language", "isOriginalTitle", "region")
+df1 = spark.read.option('header', 'true').csv("title.akas.tsv", sep=r'\t')
+df1 = df1.withColumn("titleId", col("titleId").cast(StringType()))\
+         .withColumn("ordering", col("ordering").cast(IntegerType()))\
+         .withColumn("title", col("title").cast(StringType()))
+df1 = df1.drop(*df1_drop_cols).filter(col("ordering") == 1) #Other way to distinct by just taking the 1 in the order. So we don't have a couple of diffrent titles
+
+df2_drop_cols = ("numVotes")
+df2 = spark.read.option('header','true').csv("title.ratings.tsv", sep=r'\t')
+df2 = df2.withColumn("tconst",col("tconst").cast(StringType()))\
+        .withColumn("averageRating", col("averageRating").cast(DoubleType()))
+df2 = df2.drop(*df2_drop_cols)
+
+df3_drop_cols = ("startYear", "endYear", "isAdult", "primaryTitle", "originalTitle", "titleType")
+df3 = spark.read.option('header','true').csv("title.basics.tsv", sep=r'\t') 
+df3 = df3.withColumn("tconst",col("tconst").cast(StringType()))\
+         .withColumn("runtimeMinutes",col("runtimeMinutes").cast(IntegerType()))\
+         .withColumn("genres",col("genres").cast(StringType()))
+df3 = df3.drop(*df3_drop_cols)
+
+joined_df = df1.join(df2, df1.titleId == df2.tconst, "left")
+df1.unpersist(), df2.unpersist()
+joined_df = joined_df.join(df3, joined_df.titleId == df3.tconst, "left").orderBy("titleId")
+df3.unpersist()
+DF = joined_df.drop("tconst", "ordering", "titleID")
 
 rating_level = float(0.0)
-if len(sys.argv) == 2 or len(sys.argv) == 0:
+if len(sys.argv) == 4 or len(sys.argv) == 1:
+        rating_level = ""
+        runtime_min = ""
+        runtime_max  = ""
         try:
                 rating_level = float(sys.argv[1])
-                runtime_max = float(sys.argv[2])
+                runtime_min = float(sys.argv[2])
+                runtime_max = float(sys.argv[3])
         except:
-                print("Input wasent a Number")
+                print("No or wronge Input, default will be assumend")
                 rating_level = float(0.0)
-                runtime_max = float(100000000000)
+                runtime_min = float(1)
+                runtime_max = float(10000)
         finally:
-                #AVG rungtime in a genre 
-                DF_avg_runtime_genre = DF.groupBy("genres").agg(F.avg(DF.runtimeMinutes)).orderBy("F.avg(DF.runtimeMinutes)")
-                DF_= DF.filter(DF.runtimeMinutes < runtime_max).groupBy("titleId").filter(DF.averageRating > rating_level)
-elif (len(sys.argv) == 3):
+                #Best movies in a given min./max. time 
+                Best_movie_runtime = DF.filter(DF.runtimeMinutes <= runtime_max)\
+                                        .filter(DF.runtimeMinutes >= runtime_min)\
+                                        .filter(DF.averageRating >= rating_level)\
+                                        .orderBy(DF.averageRating.desc(), DF.runtimeMinutes.desc())
+                #Check the output format
+                Best_movie_runtime.show()
+
+#This is more for getting insides of the diffrent categories
+elif (len(sys.argv) == 5):
+        rating_level = ""
+        runtime_min = ""
+        runtime_max  = ""
+        args = ""
         try:
             rating_level = float(sys.argv[1])
-            runtime_max = float(sys.argv[2])
-            if (sys.argv[3] == "yes"): genre = True 
-            else: genre = False 
+            runtime_min = float(sys.argv[2])
+            runtime_max = float(sys.argv[3])
+            args = sys.argv[4]
         except:
-                print("Input wasent a Number")
+                print("No or wronge Input, default will be assumend")
                 rating_level = float(0.0)
+                runtime_min = float(1)
+                runtime_max = float(10000)
+                args = "-avg"
         finally:
-                #AVG rungtime in a genre 
-                DF_avg_runtime_genre = DF.groupBy("genres").agg(F.avg(DF.runtimeMinutes)).orderBy("F.avg(DF.runtimeMinutes)") 
+                #Agg's of rungtime 
+                if runtime_min != 0 or runtime_max != 0:
+                        DF_agg = DF.filter(DF.runtimeMinutes <= runtime_max)\
+                                        .filter(DF.runtimeMinutes >= runtime_min)\
+                                        .filter(DF.averageRating >= rating_level)
+                elif runtime_min == 0 and runtime_max == 0 and rating_level == 0:
+                        DF_agg = DF
+                elif runtime_min == 0 and runtime_max == 0 and rating_level != 0:
+                        DF_agg = DF.filter(DF.averageRating >= rating_level)
+                else: DF_agg = DF
+
+                if args == "-avg":
+                        DF_agg = DF_agg.groupBy("genres")\
+                                       .agg(F.avg(DF.runtimeMinutes))\
+                                       .orderBy(col("avg(runtimeMinutes)").desc())
+                elif args == "-max":
+                         DF_agg = DF_agg.groupBy("genres")\
+                                        .agg(F.max(DF.runtimeMinutes))
+                         DF_agg = DF_agg.orderBy(col("max(runtimeMinutes)").desc())
+                elif args == "-min":
+                        DF_agg = DF_agg.groupBy("genres")\
+                                       .agg(F.min(DF.runtimeMinutes))\
+                                       .orderBy(col("min(runtimeMinutes)").asc())                
+                elif args == "-sum":
+                        DF_agg = DF_agg.groupBy("genres")\
+                                       .agg(F.sum(DF.runtimeMinutes))\
+                                       .orderBy(col("sum(runtimeMinutes)").desc())
+                else: 
+                        print("\n\nYou did not select a correct command!\nPossible: \n-avg \n-sum \n-min \n-max \n")
+                        exit()
+                DF_agg.printSchema()
+                #Check the output format
+                DF_agg.show()
