6978390805023e6187fdd48f62fd1157947812b7
GitWAMH <61278893+GitWAMH@users.noreply.github.com>
2021-12-09 00:39:44 +0100

    Update best_rated_movies_by_year.py

--- scripts/best_rated_movies_by_year.py
+++ scripts/best_rated_movies_by_year.py
@@ -16,26 +16,24 @@ movies_to_show = 5 if len(sys.argv)<4 else int(sys.argv[3])
 # When the user wants to use the MovieLens datasets
 if mode == '-m':
 #Obtains the dataframes from the datasets ratings.csv and movies.csv
-        df1 = spark.read.option('header','true').csv('ml-20m/ratings.csv')
-        df2 = spark.read.option('header','true').csv('ml-20m/movies.csv')
+	df1 = spark.read.option('header','true').csv('ml-20m/ratings.csv')
+	df2 = spark.read.option('header','true').csv('ml-20m/movies.csv')
 
-        list_movies = df1.join(df2,'movieId','rightouter') \
-                .withColumn('rating', col('rating').cast('float')) \
+	list_movies = df1.join(df2,'movieId','rightouter') \
+		.withColumn('rating', col('rating').cast('float')) \
 		.withColumn('year', (1970 + (col('timestamp')/(60*60*24*365))).cast('int')) \
-                .select('movieId', 'rating', 'title', 'year') \
-                .filter(col('year')==year) \
-                .groupBy('movieId','title').avg('rating') \
-                .orderBy(col('avg(rating)').desc(),col('title').asc()) \
-                .limit(movies_to_show) \
-		.show()
-                .rdd \
-                .map(lambda row: (row[1],row[2])) \
-                .collect()
+		.select('movieId', 'rating', 'title', 'year') \
+		.filter(col('year')==year) \
+		.groupBy('movieId','title').avg('rating') \
+		.orderBy(col('avg(rating)').desc(),col('title').asc()) \
+		.limit(movies_to_show) \
+		.map(lambda row: (row[1],row[2])) \
+		.collect()
 
-        print(list_movies)
+	print(list_movies)
 elif mode == '-i':
 	df1 = spark.read.csv('title.basics.tsv',sep=r'\t',header=True)
-        df2 = spark.read.csv('title.ratings.tsv',sep=r'\t',header=True)
+	df2 = spark.read.csv('title.ratings.tsv',sep=r'\t',header=True)
 	list_movies = df1.join(df2,'tconst','rightouter') \
 		.select('tconst', 'averageRating', 'originalTitle', 'genre') \
 		.filter(col('startYear')==genre) \
@@ -44,6 +42,6 @@ elif mode == '-i':
 		.map(lambda row: (row[2],row[1])) \
 		.collect()
 
-        print(list_movies)
+	print(list_movies)
 else:
 	print('Mode not selected\n');
